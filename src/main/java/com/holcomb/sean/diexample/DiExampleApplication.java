511fcd7e85673bb4b755cab94fb2e13737fd727e
Sean Holcomb <seanholcomb@gmail.com>
2020-04-23 21:39:35 -0700

    initialize controller

--- src/main/java/com/holcomb/sean/diexample/DiExampleApplication.java
+++ src/main/java/com/holcomb/sean/diexample/DiExampleApplication.java
@@ -1,13 +1,20 @@
 package com.holcomb.sean.diexample;
 
+import com.holcomb.sean.diexample.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DiExampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiExampleApplication.class, args);
-    }
+        ApplicationContext ctx = SpringApplication.run(DiExampleApplication.class, args);
 
+        MyController myController = (MyController) ctx.getBean("myController");
+
+        String greeting = myController.sayHello();
+
+        System.out.println(greeting);
+    }
 }
